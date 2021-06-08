d23c1a1b5834025e7d2cecbc3845fd689a96eb06
KengAcethylene <keng2120@gmail.com>
2021-06-08 20:12:46 +0700

    new schema file

---
+++ db/schema/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+
+const userSchema = mongoose.Schema({
+    name: { type: String, required: [true, 'Please add a name'] },
+    email: {
+        type: String,
+        validate: {
+            validator: (val) => /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(val),
+            message: (props) => `${props.value} validation error`,
+        },
+    },
+
+    password: {
+        type: String,
+        required: [true, 'Please add a password'],
+        select: false,
+    },
+    createdAt: { type: Date, default: new Date() }
+}, { collection: "User" });
+
+userSchema.methods.getSignedJwtToken = function () {
+    return jwt.sign({ user_id: this._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRE,
+    });
+};
+
+const userModel = mongoose.model("User", userSchema);
+
+module.exports = userModel;
